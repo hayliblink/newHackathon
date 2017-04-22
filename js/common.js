ef938a0db7269131cca95fb1db366832d766e8d4
ignis33 <julien.leotier@gmail.com>
2017-04-22 19:22:57 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,53 @@
+(function($) {
+
+
+	$(document).ready(function() {
+		fullWidthHeight();
+		getSetImage();
+		pageWrapperHeight();
+		mobileNav();
+	});
+	
+	$(window).resize(function() {
+		fullWidthHeight();
+		pageWrapperHeight();
+	});
+
+
+	function fullWidthHeight(){
+		var w = $(window).width();
+		var h = $(window).height();
+		$(".main-wrapper").css({
+			width : w,
+			"min-height": h
+		});
+	}
+
+	function getSetImage(){
+		var path = $(".bg-wrapper").attr("data-mainbg");
+		$(".main-wrapper.bg-wrapper").css({
+			"background-image" : "url("+path+")"
+		});
+	}
+
+	function pageWrapperHeight(){
+		var total = $(".main-wrapper").height();
+		var footer = $(".front-display").outerHeight();
+		var page = total - footer;
+		if($("html").hasClass("ff")){
+			$(".page-wrapper").css("height", page);
+			$(".page-wrapper").css("min-height", page);
+		}
+		else{
+			$(".page-wrapper").css("min-height", page);
+		}
+	}
+})(jQuery);
+
+
+
+
+
+
+
+
