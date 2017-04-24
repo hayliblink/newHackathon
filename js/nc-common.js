75a7badf0b647b1fc404dca090d5d2d9cc67a441
JulienLeotier <julien.leotier@gmail.com>
2017-04-24 17:35:32 +0200

    menu hamburger et compteur

--- js/nc-common.js
+++ js/nc-common.js
@@ -1,16 +1,4 @@
-/**
-*****************************************************************
-* PROJECT : NC-Attract Coming-Soon Page
-* AUTHOR : NCodeArt
-*****************************************************************
-*/
 
-/**
-*****************************************************************
-* This file is licensed to NCodeArt. 
-* it's not allowed to copy or reuse it Copyright NCodeArt 2015
-*****************************************************************
-*/
 
 var nc = {};
 var package_ver = 'v0.5';
@@ -44,7 +32,7 @@ nc.eqh = function(parentObj, childObj, a) {
 				$(this).equalize({
 					target: $(childObj)
 				});
-			};	
+			};
 		});
 	};
 }
@@ -54,7 +42,7 @@ nc.getvar = function (v, default_v, val_type) {
 	'use strict';
 	if (val_type == 'n') {
 		return v ? parseInt(v,10) : default_v;
-	} 
+	}
 	if (val_type == 'b') {
 		if (v == 'true') { return true; }
 		else if (v == 'false') { return false; }
@@ -66,111 +54,8 @@ nc.getvar = function (v, default_v, val_type) {
 		} else {
 			return v ? v : default_v;
 		};
-		
+
 	}
-}
-
-/*----------  OWLCAROUSEL  ----------*/
-nc.owlitems = function (arr) {
-	'use strict';
-	if (typeof(arr) == "string" && arr != 'false') {
-		var t1 = arr.split('|');
-		var t2 = {};
-		$.each(t1, function(index, val) {
-			var str = val;
-			var newarr = str.split(',');
-			t2[newarr[0]] = {}
-			t2[newarr[0]] = {items: parseInt(newarr[1],10)};
-		});
-		return t2;
-	}else if(arr === 'false'){
-		return {};
-	}else{
-		return false;
-	}
-}
-
-nc.slider = function (owlObj) {
-	
-	'use strict';
-
-	var resObj = {
-		0    : { items:1 },
-		420  : { items:2 },
-		600  : { items:3 },
-		768  : { items:3 },
-		980  : { items:4 }
-	}
-
-	var owlEle = $(owlObj + ' .owl-carousel');
-
-	var config = {
-		center             : nc.getvar($(owlObj).attr('data-center'), false, 'b'),
-		stagePadding       : nc.getvar($(owlObj).attr('data-stpd'), 0, 'n'),
-		items              : nc.getvar($(owlObj).attr('data-items'), 5, 'n'),
-		margin             : nc.getvar($(owlObj).attr('data-margin'), 0, 'n'),
-		nav                : nc.getvar($(owlObj).attr('data-nav'), false, 'b'),
-		dots               : nc.getvar($(owlObj).attr('data-pager'), false, 'b'),
-		slideby            : nc.getvar($(owlObj).attr('data-slideby'), 1, 'n'),
-		rbase              : nc.getvar($(owlObj).attr('data-rbase'), $(owlObj).parent(), 's'),
-		res                : $(owlObj).attr('data-itemrange') ? nc.owlitems($(owlObj).attr('data-itemrange')) : resObj,
-		animOut            : nc.getvar($(owlObj).attr('data-out'), 'fadeOut', 's'),
-		animIn             : nc.getvar($(owlObj).attr('data-in'), 'fadeIn', 's'),
-		autoplay           : nc.getvar($(owlObj).attr('data-autoplay'), false, 'b'),
-		autoplayTimeout    : nc.getvar($(owlObj).attr('data-timeout'), 3000, 'n'),
-		autoplayHoverPause : nc.getvar($(owlObj).attr('data-hstop'), true, 'b'),
-		loop               : nc.getvar($(owlObj).attr('data-loop'), false, 'b'),
-		autoWidth          : nc.getvar($(owlObj).attr('data-awidth'), false, 'b'),
-		autoHeight         : nc.getvar($(owlObj).attr('data-hauto'), true, 'b'),
-		touchDrag          : nc.getvar($(owlObj).attr('data-tdrag'), true, 'b'),
-		mouseDrag          : nc.getvar($(owlObj).attr('data-mdrag'), true, 'b'),
-		pullDrag           : nc.getvar($(owlObj).attr('data-pdrag'), true, 'b'),
-		contentHeight      : nc.getvar($(owlObj).attr('data-h'), true, 'b')
-	}
-	$(owlObj).animate({opacity:1}, 300, function(){
-
-		 owlEle.owlCarousel({
-			center                : config.center,
-			stagePadding          : config.stagePadding,
-			items                 : config.items,
-			margin                : config.margin,
-			nav                   : config.nav,
-			dots                  : config.dots,
-			slideBy               : config.slideby,
-			navText               : ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-			responsiveBaseElement : config.rbase,
-			responsive            : config.res,
-			loop                  : $(owlObj+" .owl-carousel > .item").length > 1 ? config.loop : false,
-			animateOut            : config.animOut, //'slideOutDown',
-			animateIn             : config.animIn, //'flipInX',
-			autoplay              : config.autoplay,
-			autoplayTimeout       : config.autoplayTimeout,
-			autoplayHoverPause    : config.autoplayHoverPause,
-			autoHeight            : config.autoHeight,
-			autoWidth             : config.autoWidth,
-			touchDrag             : config.touchDrag,
-			mouseDrag             : config.mouseDrag,
-			pullDrag              : config.pullDrag,
-			autoplaySpeed : 2000,
-
-			onInitialized: function () {
-				owlEle.animate({opacity: 1}, 300);
-				if (owlEle.find('.feedback-box1').length > 0) {
-					config.contentHeight ? nc.eqh(owlEle, ".feedback-box1", "") : false;
-				}
-				if (owlEle.find('.feedback-box3').length > 0) {
-					config.contentHeight ? nc.eqh(owlEle, ".feedback-box3 .feedback", "") : false;
-				}
-				if (owlEle.find('.feedback-box4').length > 0) {
-					config.contentHeight ? nc.eqh(owlEle, ".feedback-box4 .feedback", "") : false;
-				}
-			}
-		});
-
-		$(owlObj).find('.carousel-btn .prev').on('click', function() { owlEle.trigger('prev.owl.carousel'); });
-		$(owlObj).find('.carousel-btn .next').on('click', function() { owlEle.trigger('next.owl.carousel'); });
-
-	});
 }
 
 /*----------  COUNTDOWN-CLOCK  ----------*/
@@ -185,49 +70,63 @@ nc.countdown = function (obj) {
 		min   : parseInt($(obj).attr("data-min"),10),
 		sec   : parseInt($(obj).attr("data-sec"),10)
 	}
-	
+
 	var oneDay     = 24*60*60*1000; // hours*minutes*seconds*milliseconds
 	var firstDate  = new Date(config.year, config.month-1, config.day-1);
 	var d          = new Date();
 	var secondDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
 	var diffDays   = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-	
-	var countdownHtml  = '<div class="inner-dashboard">';
-		countdownHtml += '	<!-- DAYS -->';
-		countdownHtml += '	<div class="dash days_dash">';
-		countdownHtml += '		<div class="inner-dash">';
-		countdownHtml += diffDays > 99 ? '<div class="digit">0</div>' : '';
-		//countdownHtml += '<div class="digit">0</div>';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '		</div>';
-		countdownHtml += '		<span class="dash_title">Jours</span>';
-		countdownHtml += '	</div>';
-		countdownHtml += '	<!-- HOURS -->';
-		countdownHtml += '	<div class="dash hours_dash">';
-		countdownHtml += '		<div class="inner-dash">';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '		</div>';
-		countdownHtml += '		<span class="dash_title">Heures</span>';
-		countdownHtml += '	</div>';
-		countdownHtml += '	<!-- MINIUTES -->';
-		countdownHtml += '	<div class="dash minutes_dash">';
-		countdownHtml += '		<div class="inner-dash">';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '		</div>';
-		countdownHtml += '		<span class="dash_title">Min</span>';
-		countdownHtml += '	</div>';
-		countdownHtml += '	<!-- SECONDS -->';
-		countdownHtml += '	<div class="dash seconds_dash">';
-		countdownHtml += '		<div class="inner-dash">';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '			<div class="digit">0</div>';
-		countdownHtml += '		</div>';
-		countdownHtml += '		<span class="dash_title">Sec</span>';
-		countdownHtml += '	</div>';
-		countdownHtml += '</div>';
+	if ( d.getFullYear() == config.year && d.getMonth() == config.month -1 && d.getDate() >= config.day){
+		var countdownHtml = '<div class="inner-dashboard" style="overflow: hidden">';
+				countdownHtml += '<h1> Mon message trop cool </h1></div>'
+	}
+	else if ( d.getFullYear() == config.year && d.getMonth() > config.month - 1){
+		var countdownHtml = '<div class="inner-dashboard" style="overflow: hidden">';
+				countdownHtml += '<h1> Mon message trop cool </h1></div>'
+	}
+
+	else if ( d.getFullYear() > config.year ){
+		var countdownHtml = '<div class="inner-dashboard" style="overflow: hidden">';
+				countdownHtml += '<h1> Mon message trop cool </h1></div>'
+	}
+	else{
+		var countdownHtml  = '<div class="inner-dashboard">';
+			countdownHtml += '	<!-- DAYS -->';
+			countdownHtml += '	<div class="dash days_dash">';
+			countdownHtml += '		<div class="inner-dash">';
+			countdownHtml += diffDays > 99 ? '<div class="digit">0</div>' : '';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '		</div>';
+			countdownHtml += '		<span class="dash_title">Jours</span>';
+			countdownHtml += '	</div>';
+			countdownHtml += '	<!-- HOURS -->';
+			countdownHtml += '	<div class="dash hours_dash">';
+			countdownHtml += '		<div class="inner-dash">';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '		</div>';
+			countdownHtml += '		<span class="dash_title">Heures</span>';
+			countdownHtml += '	</div>';
+			countdownHtml += '	<!-- MINIUTES -->';
+			countdownHtml += '	<div class="dash minutes_dash">';
+			countdownHtml += '		<div class="inner-dash">';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '		</div>';
+			countdownHtml += '		<span class="dash_title">Min</span>';
+			countdownHtml += '	</div>';
+			countdownHtml += '	<!-- SECONDS -->';
+			countdownHtml += '	<div class="dash seconds_dash">';
+			countdownHtml += '		<div class="inner-dash">';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '			<div class="digit">0</div>';
+			countdownHtml += '		</div>';
+			countdownHtml += '		<span class="dash_title">Sec</span>';
+			countdownHtml += '	</div>';
+			countdownHtml += '</div>';
+	}
+
 
 	$(obj).html(countdownHtml);
 
@@ -273,151 +172,7 @@ nc.animationIn = function(obj){
 	});
 }
 
-/*----------  FORM  ----------*/
-nc.global_validation = {
-	form: '',
-	rules: { 
-		email            : { required: true, email: true },
-		name             : { required: true },
-		message          : { required: true },
-		phone            : { required: true, number: true },
-		date             : { required: true, date: true },
-	},
-	msg: {
-		email: {email: "Please, enter a valid email"}
-	},
-	successMsg : "<div class='msg-success alert alert-success' role='alert'>Thank you for contact us. We will contact you as soon as possible.</div>",
-	errorMsg   : "<div class='msg-error alert alert-danger' role='alert'>Oops! Looks like something went wrong. Please try again later.</div>"
-}
 
-nc.formVaidate = function (obj) {
-	'use strict';
-	if (obj.msgpos == 'append') {
-		$(obj.form).validate({
-			onfocusout: false,
-			onkeyup: false,
-			rules: obj.rules,
-			messages: obj.msg,
-			errorPlacement: function(error, element) {
-				if (obj.msgpos == 'append') {
-					error.appendTo( element.closest("form").find('.msg-wrp'));
-				};
-			},
-			highlight: function(element) {
-				$(element)
-			},                    
-			success: function(element) {
-				element.text('').addClass('valid');
-			}
-		});
-	} else {
-		$(obj.form).validate({
-			onfocusout: false,
-			onkeyup: false,
-			rules: obj.rules,
-			messages: obj.msg,
-			highlight: function(element) {
-				$(element)
-			},                    
-			success: function(element) {
-				element.text('').addClass('valid');
-			}
-		});
-	};
-	
-}
-
-nc.resetForm = function (form) {
-	'use strict';
-	$(form).find('input[type="text"], input[type="email"], textarea').val(null);
-}
-
-nc.contactForm = function($form, formData, validate_data){
-	'use strict';
-	$form.find('.error').hide();
-	if ($form.valid()) {
-		var $btn = $(this).button('loading');
-		$.ajax({
-			url: $form.attr('action'),
-			type: 'POST',
-			data: formData,
-			success: function(data) {
-				$form.find(".msg-success").remove();
-				$form.prepend(validate_data.successMsg);
-				$btn.button('reset');
-				$form.find(".msg-success").delay(4000).fadeOut('400', function() {
-					$(this).remove();
-					$.magnificPopup.close();
-				});
-				nc.resetForm($form);
-				
-			},
-			error: function() {
-				$form.find(".msg-error").remove();
-				$form.prepend(validate_data.errorMsg);
-				$btn.button('reset');
-				$form.find(".msg-error").delay(4000).fadeOut('400', function() {
-					$(this).remove();
-					$.magnificPopup.close();
-				});
-			}
-		});
-	} else {
-		$form.find(".error").delay(2000).fadeOut('400', function() {
-			$(this).remove();
-		});	
-	};
-}
-
-nc.formWidget = function (obj) {
-	'use strict';
-	var config = {
-		popup_selector : $(obj).attr('data-popup') ? '.'+$(obj).attr('data-popup') : false,
-		form_selector  : obj,
-	}
-	var $form = $(config.form_selector);
-
-	// Validation rules
-	nc.global_validation.form = config.form_selector;
-	var validate_data = nc.global_validation;
-	
-	// Form validation
-	nc.formVaidate(validate_data);
-
-	// Pop up form
-	// if (config.popup_selector) {
-	// 	nc.inlinePopup(config.popup_selector);	
-	// };
-	
-	// Form
-	$form.find('button').off('click').on('click', function(e) {
-		e.preventDefault();
-		nc.contactForm($form, $form.serializeObject(), validate_data);
-		return false;
-	});
-}
-
-$.fn.serializeObject = function() {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function() {
-		
-		// Field labels
-		var field_label = $('[name='+this.name+']').attr('data-label') ? $('[name='+this.name+']').attr('data-label') : this.name;
-
-		// Field values
-		if (o[this.name]) {
-			if (!o[this.name].push) {
-				o[this.name] = [o[this.name]];
-			}
-			o[this.name].push({val: this.value, label: field_label} || '');
-		} else {
-			//o[this.name] = this.value || '';
-			o[this.name] = {val: this.value, label: field_label} || '';
-		}
-	});
-	return o;
-};
 
 nc.common = function() {
 
@@ -428,22 +183,7 @@ nc.common = function() {
 		});
 	}
 
-	/*----------  OWLCAROUSEL  ----------*/
-	if (nc.elcheck(".carousel-widget")) {
-		var carousel = 0;
-		$('.carousel-widget').each(function(){
-
-			// SET ID ON ALL OBJECTS
-			carousel++;
-			var owlObj = 'owl'+carousel;
-			$(this).css({opacity:0});
-			$(this).attr("id", owlObj);
-			$(this).addClass(owlObj);
-			nc.slider("#"+owlObj);
-		});
-	}
-
-	/*----------  COUNTDOWN-CLOCK  ----------*/		
+	/*----------  COUNTDOWN-CLOCK  ----------*/
 	if (nc.elcheck(".countdown-widget")) {
 		var countdown = 0;
 		$(".countdown-widget").each(function(index, el) {
@@ -453,41 +193,6 @@ nc.common = function() {
 			countdown++;
 		});
 	}
-
-	/*----------  SUBSCRIBE  ----------*/
-	if (nc.elcheck("#subscribe")) {
-		var $subscribeForm = $('#subscribe');
-		var subscribe_validate_data = {
-			form: "#subscribe",
-			rules: { email: { required: true, email: true } },
-			msg: {
-					email: {
-						required: "Please enter email before submit.",
-						email: "Please, enter a valid email"
-					}
-				},
-			msgpos: 'append',
-			successMsg: "<div class='msg-success'>Congrats! You are in list. We will inform you as soon as we finish.</div>",
-			errorMsg: "<div class='msg-error>Oops! Looks like something went wrong. Please try again later.</div>"
-		}
-
-		$('#subscribe').off('click').on('click', '#submit', function(e) {
-			e.preventDefault();
-			var formData = {
-				email: $subscribeForm.find('input').val()
-			}
-			nc.formVaidate(subscribe_validate_data);
-			nc.contactForm($subscribeForm, formData, subscribe_validate_data);
-			return false;
-		});
-	}
-	
-	/*----------  FORM  ----------*/
-	if (nc.elcheck(".form-widget")) {
-		$(".form-widget").each(function(index, el) {
-			nc.formWidget(this);
-		});
-	};
 }
 
 /*----------  VIDEO BACKGRUND  ----------*/
@@ -516,30 +221,20 @@ nc.videoBg = function (obj, imglist) {
 
 	if( isMobile.any() ){
 		$(obj).css("display","none");
-		/*$(obj).vegas({
-			slides: [
-				{ src: "images/bg-1.jpg" },
-				{ src: "images/bg-2.jpg" },
-				{ src: "images/bg-3.jpg" },
-				{ src: "images/bg-4.jpg" }
-			]
-			slides: imglist
-		});*/
 	}
 	else{
 		$(obj).css("display","block");
 		$(obj).YTPlayer();
 	}
-}
-
-;(function(){
+};
+(function(){
 	'use strict';
 
 	$(window).load(function(){
 
 		/*----------  PAGE-LOADER  ----------*/
 		if (nc.elcheck(".page-loader-wrapper")) {
-			$(".page-loader-wrapper").fadeOut(800);	
+			$(".page-loader-wrapper").fadeOut(800);
 		}
 
 	});
@@ -564,12 +259,12 @@ nc.videoBg = function (obj, imglist) {
 				if(pageUrl == "home-page") {
 					nc.animationOut("#pages");
 
-					setTimeout(function() { 
+					setTimeout(function() {
 						$("#pages").html(" ");
 						pageWrapper.removeClass("full");
 						pageWrapper.addClass("active-home");
 
-						setTimeout(function() { 
+						setTimeout(function() {
 							if (nc.elcheck(animated)) {
 								$("#page-wrapper .home-page").removeClass("hidden");
 								$(".text-animarion").removeClass("hide-text");
@@ -586,29 +281,29 @@ nc.videoBg = function (obj, imglist) {
 							nc.animationOut(".text-animarion");
 						}
 
-						setTimeout(function() { 
+						setTimeout(function() {
 							pageWrapper.removeClass("active-home");
 							$("#page-wrapper .home-page").addClass("hidden");
 							$(".text-animarion").addClass("hide-text");
 							pageWrapper.addClass("full");
-							setTimeout(function() { 
+							setTimeout(function() {
 								$.get(pageUrl, function(data){
 									$("#pages").html(data);
 									nc.animationIn("#pages");
 									nc.common();
-								});	
-							}, delay1);		
+								});
+							}, delay1);
 						}, delay2);
 					}
 					else {
 						nc.animationOut("#pages");
-						setTimeout(function() { 
+						setTimeout(function() {
 							$("#pages").html(" ");
 							$.get(pageUrl, function(data){
 								$("#pages").html(data);
 								nc.animationIn("#pages");
 								nc.common();
-							});	
+							});
 						}, delay1);
 					}
 				}
@@ -672,10 +367,10 @@ nc.videoBg = function (obj, imglist) {
 
 		$.mediaquery("bind", "mq-key", "(min-width: 200px) and (max-width: 767px)", {
 			enter: function() {
-				
+
 			},
 			leave: function() {
-			
+
 			}
 		});
 
